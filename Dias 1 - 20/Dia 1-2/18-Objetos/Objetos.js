dd7b47562d6fca4211aebad8a4ac66d0f30544a0
Jo <josuephf@gmail.com>
2024-09-19 17:57:10 -0300

    Dia 1-2

---
+++ Dias 1 - 20/Dia 1-2/18-Objetos/Objetos.js
@@ -0,0 +1,10 @@
+
+/*
+Objetos em JavaScript são o tipo de dados mais importante e formam os blocos de construção do JavaScript moderno. Ao contrário dos tipos de dados primitivos (Número, String, Booleano, Nulo, Indefinido e Símbolo), que contêm um único valor, os objetos podem conter vários valores como propriedades. Isso torna os objetos incrivelmente úteis para agrupar dados e operações relacionadas, levando a um código mais organizado e de fácil manutenção.
+*/
+
+new Object(value)
+Object(value)
+let object_name = {
+    key_name: value
+}
